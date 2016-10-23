16615543bc85a17b1d8b3d3778473bd0fd6a9b6f
golf2109@gmail.com <golf2109@gmail.com>
2016-10-23 20:25:53 +0300

    Correct P command & add R (read register of LoRa device) command

--- transmitter/Src/main.c
+++ transmitter/Src/main.c
@@ -76,12 +76,13 @@ uint8_t	addr_lora,addr_lora1,addr_lora2,data_lora,data_lora1,data_lora2;
   unsigned char UART_Data;
   unsigned char RFM_Package[256]={0};
 
-uint8_t	read_reg;
+uint8_t	read_reg, read_register,reading_register,reading_register_l, reading_register_h,  read_register_l;
 uint8_t	F_23_16,F_15_8,F_7_0;
 uint8_t	l_100,l_10,l_1;
 uint8_t	f_100000,f_10000,f_1000,f_100,f_10,f_1;
 uint32_t freq_hex1,freq_hex2;
-uint8_t power_bits;
+uint8_t power_bits, power_reg_old;
+uint8_t bandwidth_bit, old_bandwidth_bit;	
 uint8_t spread, spread_read,spread_write,spread_mask;
 uint8_t	time_sec;
 extern	uint8_t* out1,out2;
@@ -89,7 +90,7 @@ extern	uint8_t spiTxData[2];
 extern	uint8_t spiRxData[2];
 	
 uint8_t www[6];
-
+uint8_t reg_setting[0x40];
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -294,6 +295,64 @@ dig[0] = 0x30 + hexdig/100;
 dig[1] = 0x30 + (hexdig - 100*(hexdig/100))/10;
 dig[2] = 0x30 + (hexdig - 100*(hexdig/100) - 10*((hexdig - 100*(hexdig/100))/10));
 }
+
+uint8_t convert_to_hex_digit(uint8_t digit)
+{
+  switch(digit)
+	{
+		case 0x0:
+			digit = 0x30;
+		break;
+		case 0x1:
+			digit = 0x31;		
+		break;
+		case 0x2:
+			digit = 0x32;
+		break;
+		case 0x3:
+			digit = 0x33;
+		break;
+		case 0x4:
+			digit = 0x34;
+		break;
+		case 0x5:
+			digit = 0x35;
+		break;
+		case 0x6:
+			digit = 0x36;
+		break;
+		case 0x7:
+			digit = 0x37;
+		break;
+		case 0x8:
+			digit = 0x38;
+		break;
+		case 0x9:
+			digit = 0x39;
+		break;
+		case 0xA:
+			digit = 0x41;
+		break;
+		case 0xB:
+			digit = 0x42;
+		break;
+		case 0xC:
+			digit = 0x43;
+		break;
+		case 0xD:
+			digit = 0x44;
+		break;
+		case 0xE:
+			digit = 0x45;
+		break;
+		case 0xF:
+			digit = 0x46;
+		break;		
+		default:
+			break;
+	}
+		return digit;
+}
 ////////////////////////////////////////////////////////////////////////////////////////////////
 void	hex_string(uint32_t packet_number, uint8_t* string_packet)
 {
@@ -364,6 +423,27 @@ int main(void)
   /* USER CODE END WHILE */
 
   /* USER CODE BEGIN 3 */
+		
+		
+		
+  //Setting RFM to standby
+  RFM_Write(0x01,0x81);
+  //Wait for mode ready
+  while(HAL_GPIO_ReadPin(GPIOC, DIO5) == 0){} 						
+		
+		
+  for(uint8_t  i=0; i<0x40;i++)		
+		{
+			reg_setting[i]=RFM_Read(i);		
+		}
+				
+  //Set RFM in continues receive
+  RFM_Write(0x01,0x85);
+  //Wait for mode ready
+  while(HAL_GPIO_ReadPin(GPIOC, DIO5) == 0){} 	
+		
+		
+		
 HAL_UART_Receive_DMA(&huart2, buffer_receive_UART, 12);
 //-------     TEST receive command S (send to registers)		
 		if((buffer_receive_UART[0] == 0x53) && (buffer_receive_UART[11] == 0x29)) 
@@ -554,24 +634,24 @@ HAL_UART_Receive_DMA(&huart2, buffer_receive_UART, 12);
 		
 		
 		
-//-------     TEST receive command P (set power) 1- min 1,  7 - max
-		if((buffer_receive_UART[0] == 0x50) && (buffer_receive_UART[3] == 0x29))
+//-------     TEST receive command P (set power) 00- min 1,  12 - max
+		if((buffer_receive_UART[0] == 0x50) && (buffer_receive_UART[4] == 0x29))
 		{
 						for(iii=0;iii<=15;iii++)
 							{
 								receive_UART[iii] = buffer_receive_UART[iii];
 							}		
-						power_bits = 0x7 & receive_UART[2];						
-						power_bits = power_bits << 4;
-						power_bits = 0x1F | power_bits;
 
-  //Setting RFM to standby
-  RFM_Write(0x01,0x81);
-  //Wait for mode ready
-  while(HAL_GPIO_ReadPin(GPIOC, DIO5) == 0){} 						
-		
-
-  RFM_Write(0x09,power_bits);		
+						//Setting RFM to standby
+						RFM_Write(0x01,0x81);
+						//Wait for mode ready
+						while(HAL_GPIO_ReadPin(GPIOC, DIO5) == 0){} 						
+						power_reg_old = RFM_Read(0x09);			
+						power_reg_old = 0xF0 & power_reg_old;							
+						power_bits = 10*(0x1 & receive_UART[2]) + 0xF & receive_UART[3];						
+						//power_bits = power_bits << 4;
+						power_bits = power_reg_old | power_bits;
+						RFM_Write(0x09,power_bits);		
 				
   //Set RFM in continues receive
   RFM_Write(0x01,0x85);
@@ -598,17 +678,18 @@ HAL_UART_Receive_DMA(&huart2, buffer_receive_UART, 12);
 							{
 								receive_UART[iii] = buffer_receive_UART[iii];
 							}		
-						power_bits = 0xF & receive_UART[2];						
-						power_bits = power_bits << 4;
-						power_bits = 0xB | power_bits;
-
   //Setting RFM to standby
   RFM_Write(0x01,0x81);
   //Wait for mode ready
   while(HAL_GPIO_ReadPin(GPIOC, DIO5) == 0){} 						
-		
 
-  RFM_Write(0x1D,power_bits);		
+	old_bandwidth_bit = RFM_Read(0x1D);
+	old_bandwidth_bit = 0xF0 & old_bandwidth_bit;
+	bandwidth_bit = 0xF & receive_UART[2];						
+	bandwidth_bit = bandwidth_bit << 4;
+	bandwidth_bit = old_bandwidth_bit | bandwidth_bit;		
+
+  RFM_Write(0x1D,bandwidth_bit);		
 				
   //Set RFM in continues receive
   RFM_Write(0x01,0x85);
@@ -634,11 +715,6 @@ HAL_UART_Receive_DMA(&huart2, buffer_receive_UART, 12);
 							{
 								receive_UART[iii] = buffer_receive_UART[iii];
 							}		
-						spread_read = RFM_Read(0x1E);
-						spread_mask = 0xF & receive_UART[2];
-						if(spread_mask > 6){spread_mask = 6;}
-						spread = 0x60 + (spread_mask << 4);
-						spread_write = spread | (0x0f & spread_read);
 
   //Spreading factor 6, PayloadCRC on
   //RFM_Write(0x1E,0x64);							
@@ -647,6 +723,12 @@ HAL_UART_Receive_DMA(&huart2, buffer_receive_UART, 12);
   RFM_Write(0x01,0x81);
   //Wait for mode ready
   while(HAL_GPIO_ReadPin(GPIOC, DIO5) == 0){} 						
+		
+						spread_read = RFM_Read(0x1E);
+						spread_mask = 0xF & receive_UART[2];
+						if(spread_mask > 6){spread_mask = 6;}
+						spread = 0x60 + (spread_mask << 4);
+						spread_write = spread | (0x0f & spread_read);
 		
   RFM_Write(0x1E,spread);		
 				
@@ -699,8 +781,7 @@ HAL_UART_Receive_DMA(&huart2, buffer_receive_UART, 12);
 			l_100	= 0xF & receive_UART[2];
 			l_10 = 0xF & receive_UART[3];
 			l_1	= 0xF & receive_UART[4];
-			package_length = 100*l_100+10*l_10+l_1;
-							
+			package_length = 100*l_100+10*l_10+l_1;							
 
   //Setting RFM to standby
   RFM_Write(0x01,0x81);
@@ -728,6 +809,101 @@ HAL_UART_Receive_DMA(&huart2, buffer_receive_UART, 12);
 							}										
 		}
 		
+//-------     TEST receive command R - read register
+		if((buffer_receive_UART[0] == 0x52) && (buffer_receive_UART[6] == 0x29))
+		{
+						for(iii=0;iii<=15;iii++)
+							{
+								receive_UART[iii] = buffer_receive_UART[iii];
+							}		
+		
+	  read_register_l = receive_UART[5];
+		switch(read_register_l)
+			{
+			case 0x30:
+					read_register_l = 0;
+				break;
+			case 0x31:
+					read_register_l = 1;
+				break;
+			case 0x32:
+					read_register_l = 2;
+				break;
+			case 0x33:
+					read_register_l = 3;
+				break;
+			case 0x34:
+					read_register_l = 4;
+				break;
+			case 0x35:
+					read_register_l = 5;
+				break;
+			case 0x36:
+					read_register_l = 6;
+				break;
+			case 0x37:
+					read_register_l = 7;
+				break;
+			case 0x38:
+					read_register_l = 8;
+				break;
+			case 0x39:
+					read_register_l = 9;
+				break;
+			case 0x41:
+					read_register_l = 0xA;
+				break;
+			case 0x42:
+					read_register_l = 0xB;
+				break;
+			case 0x43:
+					read_register_l = 0xC;
+				break;
+			case 0x44:
+					read_register_l = 0xD;
+				break;
+			case 0x45:
+					read_register_l = 0xE;
+				break;
+			case 0x46:
+					read_register_l = 0xF;
+				break;			
+			}
+  //Setting RFM to standby
+  RFM_Write(0x01,0x81);
+  //Wait for mode ready
+  while(HAL_GPIO_ReadPin(GPIOC, DIO5) == 0){} 						
+	read_register = 0x10*receive_UART[4] + read_register_l;
+	reading_register = RFM_Read(read_register);			
+		
+  reading_register_h = (0xF0 &reading_register) >> 4;
+	reading_register_l = (0xF & reading_register);		
+		
+		
+  //Set RFM in continues receive
+  RFM_Write(0x01,0x85);
+  //Wait for mode ready
+  while(HAL_GPIO_ReadPin(GPIOC, DIO5) == 0){} 						
+						
+						char *ok = "Register           ";			
+						for(iii=0;iii<=13;iii++)
+						{
+							transmit_UART[iii] = *(ok + iii);
+						}
+            transmit_UART[9] = receive_UART[4];
+						transmit_UART[0xA] = receive_UART[5];						
+						transmit_UART[0xB] = 0x20;
+						transmit_UART[0xC] = 0x3D;
+						transmit_UART[0xD] = 0x20;						
+						transmit_UART[0xE] = convert_to_hex_digit(reading_register_h);						
+						transmit_UART[0xF] = convert_to_hex_digit(reading_register_l);
+						HAL_UART_Transmit_DMA(&huart2, transmit_UART, 32); 									
+						
+						for(iii=0;iii<=15;iii++)
+							{
+								buffer_receive_UART[iii] = 0;
+							}																
+		}									
 		
 		
 			RFM_SendPackage(RFM_Package);
